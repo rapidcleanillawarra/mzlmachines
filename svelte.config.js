692e974cbdc3282f89e9905225ca6b338f71c12e
Joeven <marketing@rapidcleanillawarra.com.au>
2025-12-07 11:57:22 +0800

    static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// Defaults are fine: outputs to "build"
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: undefined
+		}),
+
+		// If the site will be at:
+		//   https://<user>.github.io/MZLMACHINES
+		// then base must be '/MZLMACHINES' in production.
+		//
+		// Replace "MZLMACHINES" with your repo name if different.
+		paths: {
+			base: process.env.NODE_ENV === 'development' ? '' : '/MZLMACHINES'
+		},
+
+		// Make sure everything is statically generated
+		prerender: {
+			entries: ['*']
+		}
 	}
 };
 
